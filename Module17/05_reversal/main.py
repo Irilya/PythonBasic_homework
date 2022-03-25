093d276b550c685e1c6892cf2bcbee1432745a54
Irilya <irilya@mail.ru>
2022-03-25 17:03:46 +0300

    modul_17_task_05_reversal_done

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+original_text = input('Введите строку: ')
+original_text_rvr = original_text[::-1]
+
+i = original_text.index('h')
+n = original_text_rvr.index('h')
+n = - n - 2
+
+print('Развёрнутая последовательность между первым и последним h:', original_text[n:i:-1])
+
+
