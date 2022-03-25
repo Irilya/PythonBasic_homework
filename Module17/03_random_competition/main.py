1159dc97bf23d3e2169cb41980da2c9c5437279e
Irilya <irilya@mail.ru>
2022-03-25 16:09:25 +0300

    modul_17_task_03_random_competition_done

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+import random
+
+first_command = [round(random.uniform(5.0, 10.0), 2) for x in range(20)]
+second_command = [round(random.uniform(5.0, 10.0), 2) for y in range(20)]
+winner_list = [(first_command[i] if first_command[i] > second_command[i]
+                else second_command[i])
+                for i in range(20)]
+
+
+print('Первая команда: ', first_command)
+print('Вторая команда: ', second_command)
+print('Победители тура: ', winner_list)
+
