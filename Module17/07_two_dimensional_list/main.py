d8917f1e2afd6e98663bca3809b04055577371d3
Irilya <irilya@mail.ru>
2022-03-26 21:23:40 +0300

    modul_17_task_09_list_of_lists_2_done

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,6 +1,6 @@
 
 generated_list = [[x for x in range(i + 1, 13, 4)] for i in range(0, 4)]
 
-print(a)
+print(generated_list)
 
 
--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,12 @@
+
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+nice_list_after = [nice_list[i][n][j] for i in range(len(nice_list))
+              for n in range(len(nice_list[i]))
+              for j in range(len(nice_list[i][n]))]
+
+print(nice_list_after)
+
+
