41d7a9771e76f048285835d9897f11a77aa58367
Irilya <irilya@mail.ru>
2022-03-26 22:47:17 +0300

    modul_17_task_10_caesar_cipher_done

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+alf_list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+            'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+            'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+original_text = input('Введите сообщение: ')
+original_text_code = [letter for letter in original_text]
+code_shift = int(input('Введите сдвиг: '))
+
+
+def text_codding(text):
+
+
+    for i in range(len(original_text_code)):
+        for j in range(len(alf_list)):
+            if original_text_code[i] == alf_list[j]:
+                original_text_code[i] = alf_list[(j + code_shift) % 33]
+                break
+
+
+text_codding(original_text)
+
+
+print('Зашифрованное сообщение: ', ''.join(original_text_code))
+
+
+
+
