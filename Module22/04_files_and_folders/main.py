de06a6261f17b1f00d3ac013c83971924b38405e
Irilya <irilya@mail.ru>
2022-05-18 19:58:05 +0300

    modul_22_all_tasks_done

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import os
+
+
+def file_directory_count(work_path):
+
+    count_dir = 0
+    count_file = 0
+    file_size = 0
+    for i_elem in os.listdir(work_path):
+        path = os.path.join(work_path, i_elem)
+        if os.path.isfile(path):
+            file_size += os.path.getsize(path)/1024
+            count_file += 1
+            count_dir += 0
+        elif os.path.isdir(path):
+
+            d_count, count, size = file_directory_count(path)
+            count_file += count
+            count_dir += d_count + 1
+            file_size += size
+
+    return count_dir, count_file, file_size
+
+
+abs_path = os.path.abspath(os.path.join(os.path.sep, 'SkillBox'))
+dir_count, file_count, size_files = file_directory_count(abs_path)
+print(f'\n{abs_path}')
+print(
+    f'\nРазмер каталога (в Кб): {size_files}'
+    f'\nКоличество подкаталогов: {dir_count}'
+    f'\nКоличество файлов: {file_count}'
+    )
--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+import zipfile
+
+
+def letters_analysis(zfile):
+
+    roman_text_zip = zipfile.ZipFile(zfile, 'r')
+    path = ''
+    for file_info in roman_text_zip.infolist():
+        path = file_info.filename
+    roman = open(roman_text_zip.extract(path), 'r', encoding='utf-8')
+    letters_value = {}
+    for line in roman:
+        for sign in line:
+            if sign.isalpha():
+                if sign not in letters_value:
+                    letters_value[sign] = 1
+                else:
+                    letters_value[sign] += 1
+    roman.close()
+    roman_text_zip.close()
+
+    return letters_value
+
+
+def sorted_letter_table(letters_dic):
+
+    value_table = []
+    letters_count = sorted(letters_dic.values())
+    for item in letters_count:
+        for key, value in letters_dic.items():
+            new_str = f'{key} - {value}\n'
+            if value == item and new_str not in value_table:
+                value_table.append(new_str)
+
+    value_table = ''.join(value_table)
+    print(value_table)
+    analys = open('letters_table.txt', 'w')
+    analys.write(value_table)
+    analys.close()
+
+
+dic_letters = letters_analysis('voyna-i-mir.zip')
+sorted_letter_table(dic_letters)
