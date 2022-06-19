8dd1efced782ae8e1aaa45b5a21d21b06ece55db
Ирина Лясковская <irilya@mail.ru>
2022-06-19 14:48:17 +0000

    Update main.py

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -2,38 +2,36 @@ import random
 
 
 def f(x, y):
-    x += random.randint(0, 10)
-    y += random.randint(0, 5)
-    return x / y
+
+    x += random.randint(0, 5)
+    y += random.randint(0, 10)
+    return round((x / y), 3)
 
 
 def f2(x, y):
-    x -= random.randint(0, 10)
-    y -= random.randint(0, 5)
-    return y / x
+
+    x -= random.randint(0, 5)
+    y -= random.randint(0, 10)
+    return round((y / x), 3)
 
 
+count_line = 0
 try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+    with open('coordinates.txt', 'r') as file:
+        with open('result.txt', 'a') as file_2:
+            for line in file:
+                count_line += 1
+                nums_list = line.split()
+                try:
+                    res1 = f(int(nums_list[0]), int(nums_list[1]))
+                    res2 = f2(int(nums_list[0]), int(nums_list[1]))
+                    number = random.randint(0, 100)
+                    my_list = sorted([str(res1), str(res2), str(number)])
+                    my_list = '    '.join(my_list)
+                    file_2.write(f'\nСтрока номер {count_line}: {my_list}')
+                except ZeroDivisionError:
+                    file_2.write(f'\nВ строке {count_line} произошло деление на ноль')
+                    print("Делениe на ноль невозможно")
+except FileNotFoundError:
+    print("Нет такого файла.")
 
-
-# TODO отредактировать и исправить программу
