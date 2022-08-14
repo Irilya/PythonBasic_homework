163a4364d44c36aad3019f4d132bfa73ef1783db
Irilya <irilya@mail.ru>
2022-08-14 16:58:39 +0300

    modul28_all_tasks_done

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,69 @@
-# TODO здесь писать код
+import math
+from abc import ABC
+from typing import Union
+
+
+class MyMath(ABC):
+    """
+    Абстрактный базовый класс как модуль для функций, которые можно использовать без создания
+    экземпляра класса.
+    """
+    @classmethod
+    def circle_len(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Функция расчета длины окружности"""
+        cir_len = 2 * math.pi * radius
+        return cir_len
+
+    @classmethod
+    def circle_sq(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Функция расчета площади окружности"""
+        cir_sq = math.pi * radius ** 2
+        return cir_sq
+
+    @classmethod
+    def sphere_square(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Функция расчета площади шара"""
+        sq_sph = 4 * math.pi * radius ** 2
+        return sq_sph
+
+    @classmethod
+    def sphere_volume(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Функция расчета объема шара"""
+        vol_sph = 4 / 3 * math.pi * radius ** 3
+        return vol_sph
+
+    @classmethod
+    def square_perimeter(cls, length: Union[int, float]) -> Union[int, float]:
+        """Функция расчета периметра квадрата"""
+        sq_per = 4 * length
+        return sq_per
+
+    @classmethod
+    def square_sq(cls, length: Union[int, float]) -> Union[int, float]:
+        """Функция расчета площади квадрата"""
+        sq_sq = length ** 2
+        return sq_sq
+
+    @classmethod
+    def cube_sq(cls, length: Union[int, float]) -> Union[int, float]:
+        """Функция расчета площади куба"""
+        c_sq = 6 * length ** 2
+        return c_sq
+
+    @classmethod
+    def cube_volume(cls, length: Union[int, float]) -> Union[int, float]:
+        """Функция расчета объема куба"""
+        cube_v = length ** 3
+        return cube_v
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+res_3 = MyMath.sphere_volume(8)
+print(res_1)
+print(res_2)
+print(res_3)
+#
+# Вот все-таки думаю - не лучше ли тут использовать staticmethod, учитывая,
+# что кроме вызова функций из модуля, действий больше не предвидится. Все-таки
+# экономия памяти?...
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,72 @@
-# TODO здесь писать код
+class Date:
+    """Класс Date должен:
+     - проверять числа даты на корректность;
+     - конвертировать строку даты в объект класса Date, состоящий из
+       соответствующих числовых значений дня, месяца и года.
+    """
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self) -> str:
+        return f'День: {self.day}\tМесяц: {self.month}\tГод: {self.year}'
+
+    @classmethod
+    def from_string(cls, date_string: str) -> 'Date':
+        """
+        Метод класса конвертирует строку даты в объект класса Date, состоящий из
+        соответствующих числовых значений дня, месяца и года.
+        :param date_string: str
+        :return: 'Date'
+        """
+        cls.date_string = date_string
+        day, month, year = map(int, date_string.split('-'))
+        date_ = cls(day, month, year)
+        return date_
+
+    @classmethod
+    def is_date_valid(cls, date_string: str) -> bool:
+        """
+        Метод класса проверяeт числа даты на корректность
+        """
+        day, month, year = map(int, date_string.split('-'))
+        if 0 < year <= 9999 and month != 2:
+            if month in [1, 3, 5, 7, 8, 10, 12]:
+                if day in range(1, 32):
+                    return True
+                else:
+                    print('Нет такого дня в этом месяце.')
+                    return False
+            elif month in [4, 6, 9, 11]:
+                if day in range(1, 31):
+                    return True
+                else:
+                    print('Нет такого дня в этом месяце.')
+                    return False
+            else:
+                print('Нет такого месяца в году.')
+                return False
+        elif 0 < year <= 9999 and month == 2:
+            if year % 4 == 0 and year % 100 != 0 \
+                    or year % 100 == 0 and year % 400 == 0:
+                if day in range(1, 30):
+                    return True
+                else:
+                    print('Нет такого дня в этом месяце.')
+                    return False
+            else:
+                if day in range(1, 29):
+                    return True
+                else:
+                    print('Нет такого дня в этом месяце.')
+                    return False
+        else:
+            print('Нет такой даты.')
+            return False
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('31-06-1900'))
