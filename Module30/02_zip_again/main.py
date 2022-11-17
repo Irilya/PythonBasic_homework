8010c0876a435b8786b8d55ed27194a928307914
Irilya <irilya@mail.ru>
2022-11-17 17:14:29 +0300

    modul17.7_all_tasks_done

--- Module30/02_zip_again/main.py
+++ Module30/02_zip_again/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+from typing import List
+
+
+letters: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numbers_letters: List[tuple] = list(map(lambda let, num: (let, num), letters, numbers))
+print(numbers_letters)
--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+from typing import List
+
+
+prime_list: List[int] = list(filter(lambda elem: elem > 1 and not any(elem % item == 0
+                                    for item in range(2, round(elem ** (1/2)) + 1)), range(1001)))
+
+print(prime_list)
+
+
+def prime_filter():
+    for elem in range(2, 1001):
+        for item in range(2, round(elem ** (1/2)) + 1):
+            if elem % item == 0:
+                break
+        else:
+            print(elem)
+
+
+prime_filter()
