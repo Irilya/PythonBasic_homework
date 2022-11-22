4aec347bdfd9fdce6f268832f9ab640ba27d1124
Irilya <irilya@mail.ru>
2022-11-22 12:51:40 +0300

    modul18.6_all_tasks_done

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+
+tel_list = ['9999999999', '999999-999', '99999x9999', '8978555555', '7983555555', '898888']
+
+tel_pattern = re.compile(r'\b[89]\d?\d{9}\b')
+count = 0
+for string in tel_list:
+    count += 1
+    if len(tel_pattern.findall(string)) > 0:
+        print(f'{count}-й номер: всё в порядке')
+    else:
+        print(f'{count}-й номер: не подходит')
--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import requests
+import re
+
+
+link = 'http://www.columbia.edu/~fdc/sample.html'
+result = requests.get(link)
+text_pattern = re.compile(r'<h3.*>(.*)<.*h3>')
+
+str_h3 = text_pattern.findall(result.text)
+
+for item in str_h3:
+    print(item)
